edf7392a796d834093a72f4dfe4e72e7576cfd79
Omri Lahav <omri@cylestio.com>
2025-03-11 17:03:25 +0100

    chore: remove redundant files and directories

--- cylestio_monitor-0.1.0/cylestio_monitor/db/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Database module for Cylestio Monitor.
-
-This module provides database functionality for storing and retrieving monitoring events.
-"""
-
-from .db_manager import DBManager
-
-__all__ = ["DBManager"] 
